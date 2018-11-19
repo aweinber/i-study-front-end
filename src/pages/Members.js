6186425e23b87d7a592ec801934b0eaadfdceb4f
Alex Weinberger <alexweinberger7@gmail.com>
2018-11-18 21:34:58 -0500

    members

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
-
+import Grid from '@material-ui/core/Grid'
 
 
 class Members extends Component {
+
+
     render() {
+        const memberOne = ["Mike Senator", "Florida", "Means and Ways"];
+        const memberTwo = ["Jill Congressperson", "Maine-2", "Foreign Affairs"];
+        const members = [memberOne, memberTwo];
+
+        let memberItems = [];
+        members.forEach((member) => memberItems.push(<Grid item>{member}</Grid>));
+
         return(
-            <h1>Members</h1>
+            <div>
+                <h1>Members</h1>
+                <Grid container>
+                    {memberItems}
+                </Grid>
+            </div>
         )
     }
 }
