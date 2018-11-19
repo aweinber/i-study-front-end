e361f2d01543150d628b7b55eb2a8253551cbac2
Alex Weinberger <alexweinberger7@gmail.com>
2018-11-19 15:13:40 -0500

    new committees

--- src/pages/Committees.js
+++ src/pages/Committees.js
@@ -1,13 +1,65 @@
 import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+
 
 class Committees extends Component {
     render() {
+
+        const { classes } = this.props;
+
+        const committeeOne = ["Agriculture, Nutrition, And Forestry", "Congress 113"];
+        const committeeTwo = ["Foreign Relations", "Congress 114"];
+        const committeeThree = ["Economic", "Congress 113"];
+        const committees = [committeeOne, committeeTwo, committeeThree];
+
+        let committeeMembers = [];
+
+        //later add members to members to correct committee
+        for (let i = 0; i < committees.length; i++) {
+            let committee = committees[i];
+            let committeeCreated = [];
+            for (let j = 0; j < committee.length; j++) {
+                if (i === 0) {
+                    committeeCreated.push(<Typography className={classes.name}>{committee}</Typography>);
+                    } else {
+                    committeeCreated.push(<Typography>{committees}</Typography>);
+                    }
+            }
+
+            committeeMembers.push(<Grid item className={classes.gridItem} xs={2}>{committeeCreated}</Grid>)
+        }
+
+
         return(
-            <h1>
-                Committee Members
-            </h1>
+            <div>
+                <h1>Committee</h1>
+                <Grid container spacing={24} className={classes.gridContainer}>
+                    {committeeMembers}
+                </Grid>
+            </div>
         )
     }
 }
 
-export default Committees
+const styles = theme => ({
+    gridItem: {
+        // border: "1px solid black",
+        margin: "10px",
+        padding: "5px",
+        backgroundColor: "#bbbbc1",
+        borderRadius: "5px"
+    },
+    gridContainer: {
+        margin: "10px"
+    },
+    name: {
+        fontWeight: 'bold',
+        textDecoration: 'underline',
+        textAlign: 'center',
+        fontSize: '15px'
+    }
+});
+
+export default (withStyles)(styles)(Committees)
