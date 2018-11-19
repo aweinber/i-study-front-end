7950449581d7a0843e5d629dbccbd7c630436db7
Alex Weinberger <alexweinberger7@gmail.com>
2018-11-19 17:11:22 -0500

    adding header menu and header formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,31 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
 import { Link } from 'react-router-dom'
+import Button from '@material-ui/core/Button'
+import HeaderMenu from './HeaderMenu'
+import HomeIcon from '@material-ui/icons/Home'
 
 const styles = theme =>  ({
+    root: {
+
+    },
     appBar: {
         backgroundColor: "#274386",
         position: "static",
+
     },
     title: {
         color: 'white',
         fontFamily: 'gill sans',
-        textDecoration: 'none'
+        flex: '1'
+    },
+    iconLink: {
+        color: 'white',
+        marginRight: '5px'
+    },
+    menu: {
+        marginLeft: 'auto',
+        marginRight: '-12px'
     }
 });
 
@@ -21,13 +36,15 @@ class Header extends Component {
     render() {
         const { classes } = this.props;
         return(
-            <AppBar className={classes.appBar}>
-                <Toolbar>
-                    <Typography variant="title" className={classes.title}>
-                        <Link to="/" className={classes.title}> Bear Gov </Link>
-                    </Typography>
-                </Toolbar>
-            </AppBar>
+            <div className={classes.root}>
+                <AppBar className={classes.appBar} position="static">
+                    <Toolbar>
+                        <Link to="/" className={classes.iconLink}><HomeIcon/></Link>
+                        <Typography className={classes.title}>Bear Gov</Typography>
+                        <HeaderMenu className={classes.menu}/>
+                    </Toolbar>
+                </AppBar>
+            </div>
         )
     }
 
