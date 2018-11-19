7dd355de770c205a3a72de13377ac5deeecd35f2
Alex Weinberger <alexweinberger7@gmail.com>
2018-11-18 21:58:34 -0500

    grid items

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -11,7 +11,20 @@ class Members extends Component {
         const members = [memberOne, memberTwo];
 
         let memberItems = [];
-        members.forEach((member) => memberItems.push(<Grid item>{member}</Grid>));
+        // members.forEach((member) => memberItems.push(<Grid item>{member}</Grid>));
+        for (let i = 0; i < members.length; i++) {
+            let member = members[i];
+            let memberListed = []
+            for (let j = 0; j < member.length; j++) {  //iterate through member of
+                if (j === 0) {
+                    memberListed.push(<h2>{member[j]}</h2>);
+                }
+                else {
+                    memberListed.push(<h6>{member[j]}</h6>)
+                }
+            }
+            memberItems.push(<Grid item>{memberListed}</Grid>)
+        }
 
         return(
             <div>
