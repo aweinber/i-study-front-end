6a2798a9ee733aa8b61fa303775fbede09017c27
Alex Weinberger <alexweinberger7@gmail.com>
2018-11-18 23:03:02 -0500

    styling members

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,50 +10,6 @@ import bills from "../static/bill.jpg";
 import committee from "../static/committee.jpg";
 import congressmen from "../static/congressmen.jpg"
 
-const styles = theme => ({
-    gridContainer: {
-        justify: 'center',
-        alignItems: 'center',
-    },
-    gridItem: {
-        justify: 'center',
-        margin: '10px'
-    },
-    card: {
-        marginLeft: '50px',
-        marginRight: '50px'
-    },
-    link: {
-        color: 'blue',
-    },
-    title: {
-        textAlign: 'center',
-        fontSize: '25px',
-        margin: '5px',
-        textDecoration: 'underline'
-    },
-    subtitle: {
-        textAlign: 'center',
-        marginTop: '50px'
-    },
-    item: {
-        marginTop: '30px',
-        textAlign: 'center',
-    },
-    media: {
-        height: '400px'
-    },
-    linkBackground: {
-        height: '50px',
-        backgroundColor: '#274386'
-    },
-    linkNames: {
-        color: 'white',
-        fontSize: '16px',
-        paddingTop: '12px',
-    }
-
-});
 
 
 
@@ -112,5 +68,51 @@ class Home extends Component {
         )
     }
 }
+
+const styles = theme => ({
+    gridContainer: {
+        justify: 'center',
+        alignItems: 'center',
+    },
+    gridItem: {
+        justify: 'center',
+        margin: '10px'
+    },
+    card: {
+        marginLeft: '50px',
+        marginRight: '50px'
+    },
+    link: {
+        color: 'blue',
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: '25px',
+        margin: '5px',
+        textDecoration: 'underline'
+    },
+    subtitle: {
+        textAlign: 'center',
+        marginTop: '50px'
+    },
+    item: {
+        marginTop: '30px',
+        textAlign: 'center',
+    },
+    media: {
+        height: '400px'
+    },
+    linkBackground: {
+        height: '50px',
+        backgroundColor: '#274386'
+    },
+    linkNames: {
+        color: 'white',
+        fontSize: '16px',
+        paddingTop: '12px',
+    }
+
+});
+
 
 export default withStyles(styles)(Home);
--- src/pages/Members.js
+++ src/pages/Members.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid'
 import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
 
-const styles = theme => ({
-    gridItem: {
-        border: "1px solid black"
-    },
-});
+
 
 class Members extends Component {
 
@@ -21,27 +18,46 @@ class Members extends Component {
 
         for (let i = 0; i < members.length; i++) {
             let member = members[i];
-            let memberListed = []
+            let memberListed = [];
             for (let j = 0; j < member.length; j++) {  //iterate through member of
                 if (j === 0) {
-                    memberListed.push(<h2>{member[j]}</h2>);
+                    memberListed.push(<Typography className={classes.name}>{member[j]}</Typography>);
                 }
                 else {
-                    memberListed.push(<h6>{member[j]}</h6>)
+                    memberListed.push(<Typography>{member[j]}</Typography>);
                 }
             }
-            memberItems.push(<Grid item className={classes.gridItem}>{memberListed}</Grid>)
+            memberItems.push(<Grid item className={classes.gridItem} xs={2}>{memberListed}</Grid>)
         }
 
         return(
             <div>
                 <h1>Members</h1>
-                <Grid container>
+                <Grid container spacing={24} className={classes.gridContainer}>
                     {memberItems}
                 </Grid>
             </div>
         )
     }
 }
+
+const styles = theme => ({
+    gridItem: {
+        // border: "1px solid black",
+        margin: "10px",
+        padding: "5px",
+        backgroundColor: "#bbbbc1",
+        borderRadius: "5px"
+    },
+    gridContainer: {
+        margin: "10px"
+    },
+    name: {
+        fontWeight: 'bold',
+        textDecoration: 'underline',
+        textAlign: 'center',
+        fontSize: '15px'
+    }
+});
 
 export default (withStyles)(styles)(Members);
