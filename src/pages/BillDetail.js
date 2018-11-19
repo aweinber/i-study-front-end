2d118bff5adc82016865b0c5ee3587007bcfa1f4
Alex Weinberger <alexweinberger7@gmail.com>
2018-11-19 16:10:24 -0500

    fixed merge conflicts

--- src/pages/BillDetail.js
+++ src/pages/BillDetail.js
@@ -1,40 +1,4 @@
 import React, { Component } from 'react'
-<<<<<<< HEAD
-import Typography from "@material-ui/core/Typography/Typography";
-import Grid from "@material-ui/core/Grid/Grid";
-import {withStyles} from "@material-ui/core";
-
-
-class BillDetail extends Component {
-    render() {
-        const { classes } = this.props;
-
-        const memberOne = ["Mike Senator", "  -Florida", "  -Means and Ways"];
-        const memberTwo = ["Jill Congressperson", "  -Maine-2", "  -Foreign Affairs"];
-        const members = [memberOne, memberTwo];
-
-        let memberItems = [];
-
-        for (let i = 0; i < members.length; i++) {
-            let member = members[i];
-            let memberListed = [];
-            for (let j = 0; j < member.length; j++) {  //iterate through member of
-                if (j === 0) {
-                    memberListed.push(<Typography className={classes.name}>{member[j]}</Typography>);
-                }
-                else {
-                    memberListed.push(<Typography>{member[j]}</Typography>);
-                }
-            }
-            memberItems.push(<Grid item className={classes.gridItem} xs={2}>{memberListed}</Grid>)
-        }
-
-        return(
-            <div>
-                <h1>Members</h1>
-                <Grid container spacing={24} className={classes.gridContainer}>
-                    {memberItems}
-=======
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 import { withStyles } from '@material-ui/core/styles'
@@ -63,7 +27,6 @@ class BillDetail extends Component {
                 </h1>
                 <Grid container>
                     {formattedBills}
->>>>>>> 1e7867ab87fd3bc7a685bf3253bc4e5bd5630db4
                 </Grid>
             </div>
         )
@@ -79,20 +42,6 @@ const styles = theme => ({
         borderRadius: "5px"
     },
     gridContainer: {
-<<<<<<< HEAD
-        margin: "20px"
-    },
-    name: {
-        fontWeight: 'bold',
-        color: 'blue',
-        textDecoration: 'underline',
-        textAlign: 'center',
-        fontSize: '18px'
-    }
-});
-
-export default (withStyles)(styles)(BillDetail);
-=======
         margin: "10px"
     },
     name: {
@@ -104,4 +53,3 @@ export default (withStyles)(styles)(BillDetail);
 });
 
 export default (withStyles)(styles)(BillDetail)
->>>>>>> 1e7867ab87fd3bc7a685bf3253bc4e5bd5630db4
